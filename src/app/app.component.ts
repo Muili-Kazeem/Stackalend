1a3f9243f2d4658ae9060e77ed822b9af00d6e3c
Muili Kazeem <muiliquazeem@gmail.com>
2025-06-05 16:15:09 +0100

    feat: implemented a dashboard design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stackalend';
+
+  @HostListener('window:beforeunload', ['$event'])
+  clearLocalStorageOnRefresh(event: Event) {
+    localStorage.clear();
+  }
 }
